8e75b541d0edeca5f54eadb909f1f3f0cc5ed6ac
jinushir <ryu.jns@gmail.com>
2018-01-14 18:24:20 +0900

    setup other buttons

--- MusicalNotePad/Classes/ViewController.swift
+++ MusicalNotePad/Classes/ViewController.swift
@@ -6,11 +6,12 @@ class ViewController: UIViewController {
     var mAudioPlayer: AVAudioPlayer!
     var mLabel: UILabel!
     var mIndexList: [Int] = []
+    var mSideMargin: CGFloat = 0
+    
     let mNames: [String] = ["ド", "ド#", "レ", "レ#", "ミ", "ファ", "ファ#", "ソ", "ソ#", "ラ", "ラ#", "シ"]
     
     let ButtonWidth: CGFloat = 60
     let ButtonHeight: CGFloat = 40
-    let ButtonHMargin: CGFloat = 10
     let ButtonVMargin: CGFloat = 10
     
     let NewLineCount: Int = 4
@@ -25,8 +26,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mSideMargin = (self.view.frame.width - (ButtonWidth * CGFloat(NewLineCount))) / CGFloat(NewLineCount + 1)
+        
         setupButtons()
-        setupDeleteButton()
+        setupOtherButtons()
         setupLabel()
     }
 
@@ -65,9 +68,9 @@ class ViewController: UIViewController {
     
     private func calcPos(index: Int) -> CGPoint
     {
-        let DefaultX = 25, DefaultY = 40
+        let DefaultX = mSideMargin, DefaultY: CGFloat = 40
         var pos = CGPoint(x: DefaultX, y: DefaultY)
-        pos.x += ((ButtonWidth + ButtonHMargin) * CGFloat(index % NewLineCount))
+        pos.x += ((ButtonWidth + mSideMargin) * CGFloat(index % NewLineCount))
         pos.y += CGFloat(Int(ButtonHeight + ButtonVMargin) * (index / NewLineCount))
         
         return pos
@@ -83,13 +86,25 @@ class ViewController: UIViewController {
         addLabel(index: index)
     }
     
-    // MARK: - Delete Button
+    // MARK: - Other Buttons
     
-    private func setupDeleteButton()
+    private func setupOtherButtons()
     {
-        let btn: UIButton = createButton(tag: 0, name: "<-", pos: CGPoint(x: 55, y: 200), selector: #selector(ViewController.didTapDeleteButton(sender:)))
-        btn.backgroundColor = UIColor.blue
-        self.view.addSubview(btn)
+        let titles = ["<-", "Copy", "Clear"]
+        let colors: [UIColor] = [UIColor.blue, UIColor.magenta, UIColor.red]
+        let selectors = [#selector(ViewController.didTapDeleteButton(sender:)),
+                         #selector(ViewController.didTapCopyButton(sender:)),
+                         #selector(ViewController.didTapClearButton(sender:))]
+        for (i, color) in colors.enumerated()
+        {
+            let x = ButtonWidth * CGFloat(i) + mSideMargin * CGFloat(i + 1)
+            let btn: UIButton = createButton(tag: 0,
+                                             name: titles[i],
+                                             pos: CGPoint(x: x, y: 200),
+                                             selector: selectors[i])
+            btn.backgroundColor = color
+            self.view.addSubview(btn)
+        }
     }
     
     @objc func didTapDeleteButton(sender: UIButton)
@@ -103,6 +118,22 @@ class ViewController: UIViewController {
         
         mIndexList.removeLast()
         refreshLabel()
+    }
+    
+    @objc func didTapCopyButton(sender: UIButton)
+    {
+        playSound()
+        
+        let pasteboard = UIPasteboard.general
+        pasteboard.setValue(mLabel.text!, forPasteboardType: "public.text")
+    }
+    
+    @objc func didTapClearButton(sender: UIButton)
+    {
+        playSound()
+        
+        mLabel.text = ""
+        mIndexList.removeAll()
     }
     
     // MARK: - Label
