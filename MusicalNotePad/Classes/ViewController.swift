d8a0090c1edd1372df7a7c7f1cb0bbbc7eb3d018
jinushir <ryu.jns@gmail.com>
2018-01-14 15:00:26 +0900

    first commit

---
+++ MusicalNotePad/Classes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicalNotePad
+//
+//  Created by 地主 龍一 on 2018/01/14.
+//  Copyright © 2018年 地主 龍一. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
